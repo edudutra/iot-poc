baf9ad3f4ee6b698425665eb8c652ee3d3f0e822
Eduardo Dutra <eduardo.dutra@habibs.com.br>
2018-08-25 01:21:30 -0300

    ajuste sender

--- sender.js
+++ sender.js
@@ -1,11 +1,36 @@
 var mqtt = require('mqtt')
 var client  = mqtt.connect('mqtt://54.233.228.117', {port:1883, username:'inhaus', password:'inhaus'})
+var moment = require('moment')
 
-var numberOfClients = 160
 var interval = 500
 var active = true
 
-var clients = Array.from({length: numberOfClients}, (v, i) => `client${(i+1).toString().padStart(3, '0')}`)
+var clients = [
+  {
+    topic: 'Advantech/00D0C9FD579C/data', 
+    do: {
+      "s":9,
+      "t":"2018-08-25T03:51:42Z",
+      "q":192,
+      "c":0,
+      "do1":false,
+      "do2":false,
+      "do3":false,
+      "do4":false
+    },
+    di: {
+      "s":9,
+      "t":"2018-08-25T03:51:42Z",
+      "q":192,
+      "c":0,
+      "di1":false,
+      "di2":false,
+      "di3":false,
+      "di4":false
+    }
+  },
+]
+
 var cicle = 0
 client.on('connect', function () {
   publish()
@@ -15,16 +40,31 @@ function publish() {
   if (active) {
     cicle++
     clients.forEach(item => {
-      var message =  `${cicle}/${Date.now().toString()}`
-      var topic = `devices/${item}`
-      client.publish(topic, message, function(err) {
+
+      t = moment().format()
+      item.do.t = t
+      item.di.t = t
+      
+      message = JSON.stringify(item.do)
+      client.publish(item.topic, message, function(err) {
         if (err) {
           console.log(err)
         } 
         else {
-          console.log(`Topic: ${topic}\tMessage: ${message}`)
+          console.log(`Topic: ${item.topic}\tMessage: ${message}`)
         }
       })
+
+      message = JSON.stringify(item.di)
+      client.publish(item.topic, message, function(err) {
+        if (err) {
+          console.log(err)
+        } 
+        else {
+          console.log(`Topic: ${item.topic}\tMessage: ${message}`)
+        }
+      })
+
     });
   }
   setTimeout(publish, interval)
